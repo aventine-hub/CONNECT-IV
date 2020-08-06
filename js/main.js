fc54b37fc787b06edf1f562f4cb81bbd233482de
Chris L <c.aventine@gmail.com>
2020-08-06 14:40:06 -0500

    Pseudocode

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*----- constants -----*/
+//Define player states
+//Define win condition
+/*----- app's state (variables) -----*/
+//Let board, turn, winner
+/*----- cached element references -----*/
+//each space
+/*----- event listeners -----*/
+//each space, reset button
+/*----- functions -----*/
+
+// 1) Define required constants X
+//1.1 Define player states
+//1.2 Define win condition
+
+// 2) Define required variables used to track the state of the game X
+//2.1 board (array), turn, winner
+
+// 3) Store elements on the page that will be accessed in code more
+//than once in variables to make code more concise, readable and performant.
+// 3.1 Each spot on the 6x7 board
+
+// 4) Upon loading the app should:
+// 	4.1) Initialize the state variables
+// 	4.2) Render those values to the page
+// 	4.3) Wait for the user to click a space
+
+// 5) Handle a player clicking a space
+//5.1 Event listener for board spaces
+//5.2 Function to update the DOM and render
+
+// 6) Handle a win condition
+//  6.1 Find group of 4 on board array
+
+// 7) Handle a player clicking the replay button
+// 7.1 Event listener for replay button, initialize
