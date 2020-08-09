7ff3be9b235368d33789675ad7696fd8851cbe67
Chris L <c.aventine@gmail.com>
2020-08-08 20:52:10 -0500

    Basic layout/ game logic

--- js/main.js
+++ js/main.js
@@ -1,36 +1,89 @@
 /*----- constants -----*/
-//Define player states
-//Define win condition
+const lookup = {
+  null: "white", //<-----could be image URLs
+  "1": "black",
+  "-1": "grey",
+};
+
+const winningCombos = [];
+
 /*----- app's state (variables) -----*/
-//Let board, turn, winner
+let board, turn, winner;
+
 /*----- cached element references -----*/
-//each space
+const squaresEl = document.querySelectorAll("tr td div");
+//   const message = document.querySelector("h1");
+const replayEl = document.querySelector("#replay");
+
 /*----- event listeners -----*/
-//each space, reset button
+document.querySelector("#replay").addEventListener("click", init);
+document.querySelector(".board").addEventListener("click", handleMove);
+
 /*----- functions -----*/
+function init() {
+  board = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ];
+  turn = 1;
+  winner = null;
+  render();
+}
 
-// 1) Define required constants X
-//1.1 Define player states
-//1.2 Define win condition
+function handleMove(event) {
+  const index = parseInt(event.target.id.replace("sp", ""));
+  if (board[index]) {
+    return;
+  }
+  board[index] = turn;
+  turn *= -1;
+  winner = null; //winning();
+  render();
+}
 
-// 2) Define required variables used to track the state of the game X
-//2.1 board (array), turn, winner
-
-// 3) Store elements on the page that will be accessed in code more
-//than once in variables to make code more concise, readable and performant.
-// 3.1 Each spot on the 6x7 board
-
-// 4) Upon loading the app should:
-// 	4.1) Initialize the state variables
-// 	4.2) Render those values to the page
-// 	4.3) Wait for the user to click a space
-
-// 5) Handle a player clicking a space
-//5.1 Event listener for board spaces
-//5.2 Function to update the DOM and render
-
-// 6) Handle a win condition
-//  6.1 Find group of 4 on board array
-
-// 7) Handle a player clicking the replay button
-// 7.1 Event listener for replay button, initialize
+function render() {
+  board.forEach(function (square, idx) {
+    squaresEl[idx].style.background = lookup[square];
+  });
+}
+init();
